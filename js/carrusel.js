7c75015bdc2eba27e73bea7cec3397adcfc90fa1
saraycorpas28-jpg <saraycorpas28@gmail.com>
2025-11-12 18:31:44 -0500

    movimientoen el carru

--- js/carrusel.js
+++ js/carrusel.js
@@ -4,42 +4,27 @@ document.addEventListener("DOMContentLoaded", () => {
   const prevBtn = document.querySelector(".anterior");
   const nextBtn = document.querySelector(".siguiente");
   let index = 0;
-  let intervalo;
 
   // --- Mostrar imagen actual ---
   function showSlide(i) {
     const offset = -i * 100;
     container.style.transform = `translateX(${offset}%)`;
   }
-
-  // --- Cambio automático ---
-  function autoPlay() {
-    intervalo = setInterval(() => {
-      index = (index + 1) % slides.length;
-      showSlide(index);
-    }, 4000); // Cambia cada 4 segundos
-  }
-
-  // --- Botón anterior ---
-  prevBtn.addEventListener("click", () => {
-    index = (index - 1 + slides.length) % slides.length;
+  //boton anterior 
+  prevBtn.addEventListener("click",()=>{
+    index = (index + 1) % slides.length;
     showSlide(index);
-    reiniciarAutoPlay();
   });
-
   // --- Botón siguiente ---
   nextBtn.addEventListener("click", () => {
     index = (index + 1) % slides.length;
     showSlide(index);
-    reiniciarAutoPlay();
   });
 
-  // --- Reinicia el autoPlay si tocan las flechas ---
-  function reiniciarAutoPlay() {
-    clearInterval(intervalo);
-    autoPlay();
-  }
 
-  // --- Inicia ---
-  autoPlay();
-});
+  // --- Cambio automático ---
+  setInterval(() => {
+      index = (index + 1) % slides.length;
+      showSlide(index);
+    }, 4000); // Cambia cada 4 segundos
+  });
