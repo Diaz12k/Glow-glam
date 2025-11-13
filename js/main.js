c3ce054f440d8061ee8caff19f50b6e6ed81e0f1
Diaz12k <juandiazz2005@gmail.com>
2025-11-12 19:05:52 -0500

    Js fade-in

--- js/main.js
+++ js/main.js
@@ -81,10 +81,21 @@ btnVaciar.addEventListener('click', () => {
   productosEnCarrito = [];
   actualizarCarritoUI();
 });
-const elements = document.querySelectorAll('.fade-in');
-const observer = new IntersectionObserver(entries => {
+// fade-in ===
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 0.2
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
   entries.forEach(entry => {
-    if (entry.isIntersecting) entry.target.classList.add('visible');
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add('visible');
+    observer.unobserve(entry.target);
   });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
 });
-elements.forEach(el => observer.observe(el));
