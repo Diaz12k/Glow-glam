6a4675cd70cfc4938ce8f40f8a4e0c73368d8630
Diaz12k <juandiazz2005@gmail.com>
2025-11-12 19:14:20 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -81,21 +81,3 @@ btnVaciar.addEventListener('click', () => {
   productosEnCarrito = [];
   actualizarCarritoUI();
 });
-// fade-in ===
-const faders = document.querySelectorAll('.fade-in');
-
-const appearOptions = {
-  threshold: 0.2
-};
-
-const appearOnScroll = new IntersectionObserver(function(entries, observer) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) return;
-    entry.target.classList.add('visible');
-    observer.unobserve(entry.target);
-  });
-}, appearOptions);
-
-faders.forEach(fader => {
-  appearOnScroll.observe(fader);
-});
